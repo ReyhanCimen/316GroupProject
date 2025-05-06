075af2755141011e639db4a9d774104ec19eaa04
egemenasim <egemenersoy523@gmail.com>
2025-05-06 15:01:29 +0300

    enemy ai added
    .

---
+++ Assets/scripts/PlayerHealth.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        Debug.Log("Player took damage. Current health: " + currentHealth);
+
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        Debug.Log("Player died!");
+        // TODO: Disable controls, show game over screen, etc.
+    }
+}
